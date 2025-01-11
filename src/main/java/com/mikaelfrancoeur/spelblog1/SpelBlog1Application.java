0950095082be71accd5b04751c29948d63257066
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-01-11 14:31:30 -0500

    initial commit

---
+++ src/main/java/com/mikaelfrancoeur/spelblog1/SpelBlog1Application.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.spelblog1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpelBlog1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpelBlog1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikaelfrancoeur/spelblog1/SpelBlog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.spelblog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpelBlog1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
