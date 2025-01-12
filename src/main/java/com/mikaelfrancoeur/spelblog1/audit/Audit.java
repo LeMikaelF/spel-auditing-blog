472575a1f45e5e27583416e2433d000219afcd8a
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-01-11 22:03:04 -0500

    cleanup

--- src/main/java/com/mikaelfrancoeur/spelblog1/audit/Audit.java
+++ src/main/java/com/mikaelfrancoeur/spelblog1/audit/Audit.java
@@ -2,7 +2,6 @@ package com.mikaelfrancoeur.spelblog1.audit;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Audit {
