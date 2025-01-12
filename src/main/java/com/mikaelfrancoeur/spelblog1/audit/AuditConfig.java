65503ba4f6defbcd4cb4c4021a0b8a8055734599
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-01-11 21:52:36 -0500

    add missing @Configuration annotation

--- src/main/java/com/mikaelfrancoeur/spelblog1/audit/AuditConfig.java
+++ src/main/java/com/mikaelfrancoeur/spelblog1/audit/AuditConfig.java
@@ -1,7 +1,9 @@
 package com.mikaelfrancoeur.spelblog1.audit;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@Configuration
 @EnableAspectJAutoProxy
 class AuditConfig {
 }
